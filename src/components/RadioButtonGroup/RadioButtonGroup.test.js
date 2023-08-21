2fabdebff6f8c61ef02b937e536826663d7b99a1
C Lewis <casserolecodes@gmail.com>
2023-08-21 05:57:55 -0400

    add initial files

---
+++ src/components/RadioButtonGroup/RadioButtonGroup.test.js
@@ -0,0 +1,14 @@
+test('component renders without errors', () => {
+  // TODO implement test
+  expect(false).toBe(true);
+});
+
+test('options are correctly displayed', () => {
+  // TODO implement test
+  expect(false).toBe(true);
+});
+
+test('onChange callback is called correctly when an option is clicked', () => {
+  // TODO implement test
+  expect(false).toBe(true);
+});
---
+++ src/components/RadioButtonGroup/index.js
@@ -0,0 +1,7 @@
+export default function RadioButtonGroup({ selectedValue, options, onChange }) {
+  return (
+    <div>
+      TODO: Implement your solution here!
+    </div>
+  );
+}
